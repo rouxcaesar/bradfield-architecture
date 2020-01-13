a9169f48b571d79cf2929d8fa5f977ba1c01c6c0
rouxcaesar <jrzerwick@gmail.com>
2020-01-13 09:44:07 -0500

    Reviewed vm.rb, extended work on Go implementation of vm, and downloaded data encoding directories

--- virtual-machine/main.go
+++ virtual-machine/main.go
@@ -4,6 +4,34 @@ import (
 	"fmt"
 )
 
+const (
+	LOAD  = 0x01
+	STORE = 0x02
+	ADD   = 0x03
+	HALT  = 0xFF
+	PC    = 0x00
+	A     = 0x01
+	B     = 0x02
+)
+
+// var memory []int
+
+// var registers []int
+
+func virtualMachine(memory, registers) {
+	for memory[registers[PC]] {
+		switch op {
+		}
+	}
+}
+
 func main() {
 	fmt.Println("Hello world!")
+	fmt.Println(LOAD)
+	fmt.Println(STORE)
+	fmt.Println(ADD)
+	fmt.Println(HALT)
+	fmt.Println(PC)
+	fmt.Println(A)
+	fmt.Println(B)
 }
--- virtual-machine/vm-lab-exercise-solution/vm.rb
+++ virtual-machine/vm-lab-exercise-solution/vm.rb
@@ -14,6 +14,8 @@ def virtual_computer(memory, registers)
  # contains the halt instruction.
  while memory[registers[PC]] != HALT do
    puts "============================", "No HALT yet", "============================"
+   puts "PC: ", PC
+   puts "registers[PC]: ", registers[PC]
    puts "memory[registers[PC]]: ", memory[registers[PC]]
    puts
    op = memory[registers[PC]]
@@ -21,6 +23,9 @@ def virtual_computer(memory, registers)
    case op
    when LOAD
      puts "============================", "IN LOAD", "============================"
+     puts "PC: ", PC
+     puts "registers[PC]: ", registers[PC]
+     puts "memory[registers[PC]]: ", memory[registers[PC]]
      register_address = memory[registers[PC]]
      puts "register_address: ", register_address
      puts
@@ -30,11 +35,21 @@ def virtual_computer(memory, registers)
      puts
      registers[PC] += 1
      # load both bytes in word
+     # Have to add the 2 bytes together to get the "full value" if the value is greater than 1 byte.
+     # Ex: 3 can be represeted in one byte, so we have 3 + 0 = 3
+     # Ex: 5281 is too big for one byte, so we have 161 + (256 * 20) = 161 + 5120 = 5281
+     # memory[word_address] is the least significant byte
+     # memory[word_address + 1] is the most significant byte
      registers[register_address] = memory[word_address] + (256 * memory[word_address + 1])
+     puts "memory[word_address]: ", memory[word_address]
+     puts "(256 * memory[word_address + 1]) : ", (256 * memory[word_address + 1])
      puts "registers[register_address]: ", registers[register_address]
      puts
    when STORE
      puts "============================", "IN STORE", "============================"
+     puts "PC: ", PC
+     puts "registers[PC]: ", registers[PC]
+     puts "memory[registers[PC]]: ", memory[registers[PC]]
      register_address = memory[registers[PC]]
      puts "register_address: ", register_address
      puts
@@ -43,16 +58,23 @@ def virtual_computer(memory, registers)
      puts "word_address: ", word_address
      puts
      registers[PC] += 1
+     # We have to split our value so that it's represented by two bytes
      # write first (low order) byte
+     # This will be the least significant byte so we find the value modulo 256
      memory[word_address] = registers[register_address] % 256
      puts "memory[word_address]: ", memory[word_address]
      puts
      # write second (high order) byte
+     # This will be the most significant byte so we find the value divided by 256,
+     # which is the reverse of when we multiplied by 256
      memory[word_address + 1] = registers[register_address] / 256
      puts "memory[word_address + 1]: ", memory[word_address + 1]
      puts
    when ADD
      puts "============================", "IN ADD", "============================"
+     puts "PC: ", PC
+     puts "registers[PC]: ", registers[PC]
+     puts "memory[registers[PC]]: ", memory[registers[PC]]
      register_address_a = memory[registers[PC]]
      puts "register_address_a: ", register_address_a
      puts
@@ -73,7 +95,7 @@ memory = [
  1,1,0x10, # 0x00: load A 0x10
  1,2,0x12, # 0x03: load B 0x12
  3,1,2,    # 0x06: add A B
- 2,1,0x0e, # 0x09: store A 0x0E
+ 2,1,0x0E, # 0x09: store A 0x0E
  0xFF,     # 0x0C: halt
  0,        # 0x0D: <<unused>>
  0,0,      # 0x0E: output
